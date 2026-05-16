1d774cca165c793c1fe11e46a5437bcf28150263
朱世龙 <751328059@qq.com>
2026-05-16 17:39:16 +0800

    代码更新

---
+++ lib/components/PreviewModal.vue
@@ -0,0 +1,30 @@
+<template>
+  <div :class="{ show: visible }" class="preview-modal" @click.self="$emit('close')">
+    <div class="modal-content">
+      <button class="modal-close" @click="$emit('close')" aria-label="Close preview">×</button>
+      <div class="modal-title">{{ title }}</div>
+      <img :src="imageSrc" class="modal-image" :alt="title" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PreviewModal',
+  props: {
+    visible: {
+      type: Boolean,
+      default: false
+    },
+    imageSrc: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      default: 'PDF Preview'
+    }
+  },
+  emits: ['close']
+}
+</script>
